37a7ab75ca00be6e8d8076aeda18a85a5918c5b5
Mauricio Manoel <mauriciomanoel@gmail.com>
2017-06-16 23:59:53 +0000

    ajuste para gravar no arquivo os dados do sensor dth11

--- dht11.py
+++ dht11.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 # Carrega as bibliotecas
 import Adafruit_DHT
 import RPi.GPIO as GPIO
@@ -10,18 +12,28 @@ GPIO.setmode(GPIO.BOARD)
  
 # Define a GPIO conectada ao pino de dados do sensor
 pino_sensor = 25 # pino 22
- 
+
+# função para criar escrever no arquivo 
+def gravar_arquivo(texto):
+	arq = open('/tmp/dht11.txt', 'w');
+	arq.write(texto);
+	arq.close();
+
 # Informacoes iniciais
 print ("*** Lendo os valores de temperatura e umidade");
- 
+
 while(1):
    # Efetua a leitura do sensor
    umidade, temperatura = Adafruit_DHT.read_retry(sensor, pino_sensor);
    # Caso leitura esteja ok, mostra os valores na tela
    if umidade is not None and temperatura is not None:
+     texto = "ok" + str(temperatura) + ";" + str(umidade);	
+     gravar_arquivo(texto);
      print ("Temperatura *C= {0:0.1f}  Umidade = {1:0.1f}").format(temperatura, umidade);
-     print ("Aguarda 2 segundos para efetuar nova leitura...\n");
-     time.sleep(2)
+     print ("Aguarda 5 segundos para efetuar nova leitura...\n");
+     time.sleep(5)
    else:
      # Mensagem de erro de comunicacao com o sensor
      print("Falha ao ler dados do DHT11 !!!")
+     texto = "nok";
+     gravar_arquivo(texto);
